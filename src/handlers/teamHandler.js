4f71b3e4d96c04a93c24d0a6f3d4fd7bba9cad8f
robensky <robstrods@gmail.com>
2024-05-04 21:46:37 +0300

    Fix again (didn't account for other symbols)

--- src/handlers/teamHandler.js
+++ src/handlers/teamHandler.js
@@ -108,7 +108,7 @@ module.exports = {
                                 time: afkTime
                             });
                             rustplus.sendInGameMessage(str);
-							DiscordVoice.sendDiscordVoiceMessage(guildId, str.slice(0, -2) + ' beers');
+							DiscordVoice.sendDiscordVoiceMessage(guildId, str.slice(0, -3) + ' beers');
                             rustplus.log(client.intlGet(null, 'infoCap'), str);
                         }
                     }
